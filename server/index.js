b58aa5a9ca940c0fe9e219b3e92272b888ddbcde
Clément Ollivier <clement.o2p@gmail.com>
2016-11-17 18:27:25 +0100

    Added socket.io lib

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
 const app = express();
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use(express.static('../client'));
 
