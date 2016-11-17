a42b4677a9491fadf69c6d38a1cff576a0200964
Clément Ollivier <clement.o2p@gmail.com>
2016-11-17 18:19:52 +0100

    Serving static files in /client folder

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('../client'));
+
 app.get('/', function (req, res) {
-  res.send('Hello Snake!')
+  res.render(__dirname + 'index.html');
 });
 
 app.listen(3000, () => {
