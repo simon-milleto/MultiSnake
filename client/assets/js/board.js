464f6b67e8248634d680edbdca59d492069762d2
simon-milleto <contact@simonmilleto.com>
2016-12-01 16:51:05 +0100

    Added color to each new snake on the board

--- client/assets/js/board.js
+++ client/assets/js/board.js
@@ -11,14 +11,29 @@ export default class Board {
 		this.snakes = [];
 		this.context = createCanvasGame();
 		this.apples = [];
+		this.color = [
+			"#468966",
+			"#fff1a8",
+			"#FFB03B",
+			"#B64926",
+			"#f64804",
+			"#3032cd",
+			"#CD2C24",
+			"#13ad00",
+			"#51f1e0",
+			"#F2385A"
+		];
 	}
 
 	newSnake(x, y){
 
 
-		let snake = new Snake(this.context,x,y);
+		let snake = new Snake(this.context,x,y, this.color[0]);
 		snake.draw();
 		this.snakes.push(snake);
+
+		this.color.shift();
+
 	}
 
 	newApple(x,y){
--- client/assets/js/snake.js
+++ client/assets/js/snake.js
@@ -2,18 +2,18 @@
 
 export default class Snake {
 
-	constructor(context, x, y) {
+	constructor(context, x, y, color) {
 		this.context = context;
 		this.x = x;
 		this.y = y;
 		this.score = 0;
-
+		this.color = color;
 	}
 
 	draw() {
 		this.context.beginPath();
 		this.context.rect(this.x, this.y, 50, 50);
-		this.context.fillStyle = '#ffffff';
+		this.context.fillStyle = this.color;
 		this.context.fill();
 		this.context.closePath();
 	}
