8da12d11b1a8c468ba19a43760f2afe237692970
Bram van Osta <bramvanosta@gmail.com>
2016-12-02 18:27:14 +0100

    Added jQuery as global dependency to stop breaking Browserify

--- client/assets/js/board.js
+++ client/assets/js/board.js
@@ -3,7 +3,6 @@
 import createCanvasGame from './createCanvasGame.js';
 import Snake from './snake';
 import Apple from './apple';
-import $ from 'jquery';
 
 const DELAY = 150;
 
@@ -28,35 +27,35 @@ export default class Board {
 		];
 	}
 	newSnake(x, y) {
-		if(this.snakes.length < 10){
-			let snake = new Snake(this.context, x, y,this.getAvailableColor());
-			snake.draw();
+        if(this.snakes.length < 10){
+            let snake = new Snake(this.context, x, y, this.getAvailableColor());
+            snake.draw();
 
-		// Temp : Simulate 4 bodyParts on the snake
-			snake.addBodyPart(x - 60, y);
-			snake.addBodyPart(x - 120, y);
-			snake.addBodyPart(x - 180, y);
-		// End Temp
+            // Temp : Simulate 4 bodyParts on the snake
+            snake.addBodyPart(x - 60, y);
+            snake.addBodyPart(x - 120, y);
+            snake.addBodyPart(x - 180, y);
+            // End Temp
 
-			this.snakes.push(snake);
-		}else{
-			console.error('Error : only 10 snakes can be on the board');
-		}
-	}
+            this.snakes.push(snake);
+        } else {
+            console.error('Error : only 10 snakes can be on the board');
+        }
+    }
 
-	newApple(x, y) {
-		let apple = new Apple(this.context, x, y);
-		apple.draw();
+    newApple(x, y) {
+        let apple = new Apple(this.context, x, y);
+        apple.draw();
 
-		this.apples.push(apple);
-	}
+        this.apples.push(apple);
+    }
 
-	getAvailableColor(){
-		let snakeColor = this.snakes.map(function(snake){
+	getAvailableColor() {
+		let snakeColor = this.snakes.map(function (snake){
 			return snake.color;
 		});
 
-		let availableColor =  this.color.find(function(c) {
+		let availableColor =  this.color.find(function (c) {
 			if(!snakeColor.includes(c)){
 				return c;
 			}
@@ -70,27 +69,27 @@ export default class Board {
 			board.checkSnakeWithAppleCollision();
 		}, DELAY);
 
-		$('body').keydown(function(e) {
-			let snake = board.snakes[0];
-			if(e.keyCode === 37 && snake.direction !== 'right') {
-				snake.direction = 'left';
-			}
-			else if(e.keyCode === 38 && snake.direction !== 'down') {
-				snake.direction = 'up';
-			}
-			else if(e.keyCode === 39 && snake.direction !== 'left') {
-				snake.direction = 'right';
-			}
-			else if (e.keyCode === 40 && snake.direction !== 'up') {
-				snake.direction = 'down';
-			}
-		});
-	}
+        $('body').keydown(function (e) {
+            let snake = board.snakes[0];
+            if (e.keyCode === 37 && snake.direction !== 'right') {
+                snake.direction = 'left';
+            }
+            else if (e.keyCode === 38 && snake.direction !== 'down') {
+                snake.direction = 'up';
+            }
+            else if (e.keyCode === 39 && snake.direction !== 'left') {
+                snake.direction = 'right';
+            }
+            else if (e.keyCode === 40 && snake.direction !== 'up') {
+                snake.direction = 'down';
+            }
+        });
+    }
 
-	checkSnakeWithAppleCollision() {
-		this.snakes.forEach((snake) => {
-			snake.checkCollisionWithApples(this.apples);
-		});
-	}
+    checkSnakeWithAppleCollision() {
+        this.snakes.forEach((snake) => {
+            snake.checkCollisionWithApples(this.apples);
+        });
+    }
 
 }
--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,21 +1,21 @@
 "use strict";
 
+global.jQuery = require("jquery");
 import Board from './board';
 import createCanvasGame from './createCanvasGame.js';
 import server from './sendToServer.js';
 
-document.addEventListener('DOMContentLoaded', function() {
-	
+document.addEventListener('DOMContentLoaded', function () {
 
-	let board = new Board();
+    let board = new Board();
 
-	board.newSnake(50,50);
-	// board.newSnake(300,300);
-	board.newApple(500,350);
-	board.newApple(900,50);
-	board.newApple(800,550);
-	board.newApple(1100,200);
-	board.newApple(200,800);
+    board.newSnake(50, 50);
+    // board.newSnake(300,300);
+    board.newApple(500, 350);
+    board.newApple(900, 50);
+    board.newApple(800, 550);
+    board.newApple(1100, 200);
+    board.newApple(200, 800);
 
 	server.sendNewUser();
 	server.sendDeleteUser();
