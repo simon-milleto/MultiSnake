17fa9adfae0b7b6e1e313c5450c3e9ee4fd62da8
Romain <romain.thoreau@laposte.net>
2016-12-08 17:43:22 +0100

    On a rien vu ! bis

--- client/assets/js/scoreboard.js
+++ client/assets/js/scoreboard.js
@@ -2,16 +2,12 @@ import $ from 'jquery';
 
 export default function scoreBoard(snakes) {
 	snakes.forEach(function(e){
-		$("#userlist").append( "<li><span class='name'>" + e.name + "</span></li>" );
-
-
-		var li = $("<li>");
-		$(li).append(
+		var li = $("<li>").append(
 			$('<span>', {
 			    class: 'name',
 			    text: e.name
 			})
 		);
-
+		$("#userlist").append(li);
 	});
 }
