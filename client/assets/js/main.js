7a66c5ec16fb7b8a4c6711b7b7a8cb7f1e6a90cc
simon-milleto <contact@simonmilleto.com>
2016-12-15 14:56:59 +0100

    Apples generated by server and displayed on client side

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -9,26 +9,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	server.on('connection', function(){
 
+		$('form.username').submit(function(e) {
+			e.preventDefault();
+			var name = $(this).find('input.username')[0].value;
 
-	$('form.username').submit(function(e) {
-		e.preventDefault();
-		var name = $(this).find('input.username')[0].value;
+			$(this).remove();
 
-		$(this).remove();
-
-        let context = createCanvasGame();
-        let board = new Board(context);
+			let context = createCanvasGame();
+			let board = new Board(context);
 
         /* Random place on board for testing purpose */
 			let long = Math.floor(Math.random() * 1000);
 			let lat = Math.floor(Math.random() * 500);
 			board.newSnake(long, lat);
 
-		server.on('new_apple', function(data){
+			server.on('new_apple', function(data){
 
-			let apple = board.newApple(data.x, data.y);
-			apple.draw();
-		});
+				let apple = board.newApple(data.x, data.y);
+				apple.draw();
+			});
 
 
 			server.sendNewUser();
