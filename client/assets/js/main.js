63b854d801ab51ec6982a14920287986966be8db
Bram van Osta <bramvanosta@gmail.com>
2016-12-15 12:37:04 +0100

    Fix the problem where no snake was created [fixes #99]

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -6,46 +6,47 @@ import $ from 'jquery';
 
 document.addEventListener('DOMContentLoaded', function () {
 
-	$('form.username').submit(function(e) {
-		e.preventDefault();
-		var name = $(this).find('input.username')[0].value;
+	server.on('connection', function(){
 
-		$(this).remove();
+		$('form.username').submit(function(e) {
+			e.preventDefault();
+			var name = $(this).find('input.username')[0].value;
 
-		let board = new Board();
+			$(this).remove();
+
+			let board = new Board();
 
 
-	    // TEMP: CREATE TWO SNAKES FOR TEST PURPOSES
-		board.newSnake(0, 60, name);
-		board.newSnake(420, 330, 'Xx-JeanKevin33-xX');
-	    // END TEMP
+			// TEMP: CREATE TWO SNAKES FOR TEST PURPOSES
+			board.newSnake(0, 60, name);
+			board.newSnake(420, 330, 'Xx-JeanKevin33-xX');
+			// END TEMP
 
-	    server.on('connection', function(){
-	    	/* Random place on board for testing purpose */
-	    	let long = Math.floor(Math.random() * 1000);
-	    	let lat = Math.floor(Math.random() * 500);
-	    	board.newSnake(long, lat);
-	    });
+			/* Random place on board for testing purpose */
+			let long = Math.floor(Math.random() * 1000);
+			let lat = Math.floor(Math.random() * 500);
+			board.newSnake(long, lat);
 
-	    server.on('disconnection', function(){
-	    	// Destroy Snake
-	    });
+			// TEMP: CREATE SEVEN APPLES FOR TEST PURPOSES
+			board.newApple(765, 315);
+			board.newApple(1155, 75);
+			board.newApple(495, 615);
+			board.newApple(1035, 495);
+			board.newApple(135, 285);
+			board.newApple(1005, 795);
+			board.newApple(105, 855);
+			// END TEMP
 
-	    // TEMP: CREATE SEVEN APPLES FOR TEST PURPOSES
-		board.newApple(765, 315);
-		board.newApple(1155, 75);
-		board.newApple(495, 615);
-		board.newApple(1035, 495);
-		board.newApple(135, 285);
-		board.newApple(1005, 795);
-		board.newApple(105, 855);
-	    // END TEMP
+			server.sendNewUser();
+			server.sendDeleteUser();
+			server.sendMove();
 
-		server.sendNewUser();
-		server.sendDeleteUser();
-		server.sendMove();
+			board.render();
+		});
+	});
 
-		board.render();
+	server.on('disconnection', function(){
+		// Destroy Snake
 	});
 
 });
