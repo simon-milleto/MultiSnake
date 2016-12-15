e3d0da331af60d1e54aefea0079ac3b7a94f9552
Clément Ollivier <clement.o2p@gmail.com>
2016-12-15 09:43:02 +0100

    Prepare server to receive names

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const path = require('path')
-
+const path = require('path');
 const express = require('express');
 const app = require('express')();
 const http = require('http').Server(app);
@@ -9,6 +8,8 @@ const io = require('socket.io')(http);
 
 const totalDuration = 5000;
 const gameDuration = 3000;
+
+const playersNames = [];
 
 app.use(express.static(path.join(__dirname, '..', 'client')));
 
