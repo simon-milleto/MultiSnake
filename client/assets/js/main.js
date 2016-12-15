0abdb1acd1d4420e6900b4c34e8e85dcccf06746
Bram van Osta <bramvanosta@gmail.com>
2016-12-15 14:50:49 +0100

    Eslint

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -7,15 +7,15 @@ import $ from 'jquery';
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    server.on('connection', function(){
+	server.on('connection', function(){
 
-        $('form.username').submit(function(e) {
-            e.preventDefault();
-            var name = $(this).find('input.username')[0].value;
+		$('form.username').submit(function(e) {
+			e.preventDefault();
+			var name = $(this).find('input.username')[0].value;
 
-            $(this).remove();
+			$(this).remove();
 
-            let board = new Board();
+			let board = new Board();
 
 			/* Random place on board for testing purpose */
 			let long = Math.floor(Math.random() * (constant.CANVAS_WIDTH/30)) * 30;
@@ -23,25 +23,25 @@ document.addEventListener('DOMContentLoaded', function () {
 			board.newSnake(long, lat, name);
 
             // TEMP: CREATE SEVEN APPLES FOR TEST PURPOSES
-            board.newApple(765, 315);
-            board.newApple(1155, 75);
-            board.newApple(495, 615);
-            board.newApple(1035, 495);
-            board.newApple(135, 285);
-            board.newApple(1005, 795);
-            board.newApple(105, 855);
+			board.newApple(765, 315);
+			board.newApple(1155, 75);
+			board.newApple(495, 615);
+			board.newApple(1035, 495);
+			board.newApple(135, 285);
+			board.newApple(1005, 795);
+			board.newApple(105, 855);
             // END TEMP
 
-            server.sendNewUser();
-            server.sendDeleteUser();
-            server.sendMove();
+			server.sendNewUser();
+			server.sendDeleteUser();
+			server.sendMove();
 
-            board.render();
-        });
-    });
+			board.render();
+		});
+	});
 
-    server.on('disconnection', function(){
+	server.on('disconnection', function(){
         // Destroy Snake
-    });
+	});
 
 });
