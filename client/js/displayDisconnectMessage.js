fd4046d50a2e20297b95ce5c679b1fc37c561cf5
simon-milleto <contact@simonmilleto.com>
2016-12-15 18:22:06 +0100

    Fixe issue with jQuery

--- client/js/displayDisconnectMessage.js
+++ client/js/displayDisconnectMessage.js
@@ -3,11 +3,6 @@
 import $ from 'jquery';
 
 export default function displayDisconnectMessage() {
-	let body = $('body');
-	let box = $("#message");
-	box.html("<h2>Warning !</h2><p>You have been deconnected, please check your internet connection and try again</p>").addClass("active");
-	body.addClass("blur");
-
-	body.className += "blur";
-	box.className += "active";
+    $("#message").html("<h2>Warning !</h2><p>You have been deconnected, please check your internet connection and try again</p>").addClass("active");
+    $('body').addClass("blur");
 }
